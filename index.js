58125b65a9efb6ede59d745f6362453deae836e3
NICHOLAS R VANDERMOLEN <tree.vandermolen@gmail.com>
2020-03-28 21:29:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,13 +4,11 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
-function logShout(string){
-  string.toUpperCase
-  console.log()
+function logShout(string){  
+  console.log string.toUpperCase ()
 }
 function logWhisper(string){
-  string.toLowerCase
-  console.log()
+  console.log string.toLowerCase ()
 }
 function sayHiToGrandma (string){
   if (string.toLowerCase)
